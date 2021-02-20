3e05ac0e063dd3d619e5e7e045ea6edab612cb73
baranacikgoz <baran-acikgoz@outlook.com>
2021-02-20 22:21:38 +0300

    list and its methods have been added.

--- lists_and_list_methods.py
+++ lists_and_list_methods.py
@@ -38,10 +38,64 @@ myList = ['Asus', 'Nvidia', 'GigaByte']
 myList.append('AMD')
 myList #output: ['Asus', 'Nvidia', 'GigaByte', 'AMD']
 
+##index(), copy(), sort(), reverse(), clear()
 
-## remove() , extend(), count(), index(), copy(), sort(), reverse(), clear()
+## remove() method is used to remove a particular item from the list.
+# list.remove(item)
 
+myList = ['Asus', 'Nvidia', 'GigaByte', 'Lenovo', 'AMD']
+myList.remove('Lenovo')
+myList #output: ['Asus', 'Nvidia', 'GigaByte', 'AMD']
 
+## extend() method is used to merge two list items and store the merged items in the first list.
+#first_list.extends(second_list)
+a = ['Asus', 'Nvidia', 'GigaByte']
+b = ['Alpha', 'Gamma', 'Beta']
+a.extend(b)
+a #output: ['Asus', 'Nvidia', 'GigaByte', 'Alpha', 'Gamma', 'Beta']
+a += b #output: ['Asus', 'Nvidia', 'GigaByte', 'Alpha', 'Gamma', 'Beta']
+#**** NOTE: if we use append instead of extend the result will be list in list: ['Asus','Nvidia','GigaByte',['Alpha', 'Gamma', 'Beta']]
 
+## count() method is used to count the number times that any given item appears in a list.
+# list.count(item)
+a = [3,5,7,'ahmet',12,5,'ahmet',22,23,'veli', 'ahmet']
+a.count(5) #output: 2
+a.count("ahmet") #output: 3
 
+## index() method is used to obtain the position value of any item in the list if it exists; otherwise, it generates a ValueError.
+#list.index(item)
+a = [3,5,7,'ahmet',12,5,'ahmet',22,23,'veli', 'ahmet']
+a.index('ahmet')
+
+## copy() method is used to make a copy of a list.
+# list.copy() doesn't take any argument
+
+a = [1,2,3]
+b = [4,5,6]
+aOriginal = a.copy()
+a.extend(b)
+a #output: [1, 2, 3, 4, 5, 6]
+aOriginal #[1, 2, 3]
+ 
+## sort() method is used to sort list data.
+# list.sort() doesn't take any argument
+
+a = [12,15,2,22,17,9,7]
+a.sort()
+a #output: [2, 7, 9, 12, 15, 17, 22]
+
+## reverse() method is used to reverse the items in any list.
+# list.reverse() doesn't take any argument
+
+a = [12,15,2,22,17,9,7]
+a.sort() # [2, 7, 9, 12, 15, 17, 22]
+a.reverse()
+a #output: [22, 17, 15, 12, 9, 7, 2]
+
+## clear() method is used to remove all the items in a list and to empty lists. 
+# list.clean() doesn't take any argument
+
+a = [12,15,2,22,17,9,7]
+a.clear()
+a #output: []
 
